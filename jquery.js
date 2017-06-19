d879bc873d3e9cc7d669d0c889040b4af5a54c4c
Prince John <sprincejohn@gmail.com>
2017-06-19 13:11:55 +0530

    Initial Stage

---
+++ jquery.js
@@ -0,0 +1,56 @@
+  $(document).ready(function(){
+									// Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+
+									// Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+									// Prevent default anchor click behavior
+        event.preventDefault();
+
+									// Store hash
+		var hash = this.hash;
+
+									// Using jQuery's animate() method to add smooth page scroll
+									// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+        scrollTop: $(hash).offset().top
+        }, 900, function(){
+
+									// Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+	});
+	});
+
+
+
+	/*function myFunction() {
+		var x = document.getElementById('myDIV');
+		if (x.style.display === 'none') {
+			x.style.display = 'block';
+			$("#myDIV").show;
+		} else {
+			x.style.display = 'none';
+			$("#myDIV").hide;
+		}
+	}*/
+
+
+	$(document).ready(function(){
+
+		$(".button").on('click',function(event){
+			$('#myDIV').toggle('display');
+
+			/*if(x.style.display === 'none')
+ 			{
+				x.style.display = 'block';
+			}
+			else {
+				x.style.display = 'none';
+			}*/
+		}
+
+
+				);
+	});
